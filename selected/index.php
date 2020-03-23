711eaf105c969c8e16bdd4c06da1e537c023aea4
faouziachour <62372927+faouziachour@users.noreply.github.com>
2020-03-23 03:22:49 +0100

    Update index.php

--- selected/index.php
+++ selected/index.php
@@ -1,54 +1,58 @@
-<html><head>
-<title>Selected Shop</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<link rel="stylesheet" type="text/css" href="style.css">
+<?php
+$message="";
+ include "code.php"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+
+<head>
+
+
+
 </head>
 
-<body>
-	<div id="header">																																																																																																										<div class="inner_copy"><a href="http://www.mgwebmaster.com/free-website-builders/">free website builder</a></div>																																																																					
-		<div id="header_inside">
-			<img src="images/header.jpg" alt="setalpm" width="999" height="222" border="0" usemap="#Map"><br>																																										
-			<ul id="menu" style="background-color: #F0F8FF;">
-				<li><a href="index.php" >Home Page</a></li>
-				<li><a href="produit.php" >Nos Produits</a></li>
-				<li><a href="contacter.php" > Nous Contacter</a></li>
-				<li><a href="rapport.php" >Rapport</a></li>
-			
-			</ul>
-		</div>
-	</div>
-	<div id="wrapper">																																																																																													
-		<div id="content_inside">
-			
-			<div id="main_block">
-				<div class="about" style="align-items: center;">
-				<h1>SELECTED SHOP </h1>
-					<br>
-					<p> Bienvenue chez SELECTED SHOP,Specialises en Tshirt et Capuchons. </p>
-					<a href="produit.php">Voir Nos Produits</a>
-					
-				</div>
-				
-				
-			</div>
-		</div>
-	</div>
-	<div id="footer">
-		
-			<br><br>
-			<p> </p><center><div class="container">
-				<div class="social-links">
-					
-					
-					<a href="https://www.facebook.com/selectedshoot" target="_blank" class="facebook"><i class="fa fa-facebook"></i><span>facebook</span></a>
-					<a href="https://www.instagram.com/selectedshop1" target="_blank" class="instagram"><i class="fa fa-instagram"></i><span>instagram</span></a>
-					
-					
-				</div>
-    </div></center><p></p><center>Copyright © </center> <p></p>					                                                                                                                                                                                                         																																																				
-		
-	</div>
+<body  style="color: black; background-image:url();  background-repeat: no-repeat; background-size: cover; " >
+
+  <div class="container" style=" margin-top: 120px">
+
     
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+      
+                  <div class="text-center">
+                  	 <center>
+                     <h1 class="h4 text-gray-900 mb-4">SELECTED SITE E-COMMERCE</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Authentification</h1>
+                     </center>
+                  </div>
+                  <form class="user" action="" method="POST">
+                  	 <center>
+                    <div class="form-group">
+
+                      <input type="text" name="username" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Entrer Nom d'utilisateur..." required>
+                    </div>
+                    <div class="form-group">
+                      <input type="password" name ="password"class="form-control form-control-user" id="exampleInputPassword" placeholder="Mot de Passe" required>
+                    </div>
+                    <p class="text-center" style="color: red;">
+                                <?php echo $message;  ?>
+                    </p>
+                 
+                   <input type="submit" name="btnLogin" value="LOGIN" onclick="soumis();">
+                 </center>
+                    </a>
+                   
+                  </form>
+ 
 
 
-</body></html>
+</body>
+
+</html>
